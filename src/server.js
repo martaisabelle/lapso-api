7fbde6288408abadb427a97220ac64b0e533c023
Marta Isabelle <MARTALISBELLE@HOTMAIL.COM>
2026-06-17 17:09:18 -0300

    chore: connect to MongoDB Atlas before starting server

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@
 // Aula 9: separação entre configuração (app.js) e inicialização (server.js)
 // Aula 21: conecta ao MongoDB antes de iniciar o servidor
 import 'dotenv/config';
-import app from './src/app.js';
-import connectDB from './src/config/database.js';
+import app from './app.js';
+import connectDB from './config/database.js';
 
 const PORT = process.env.PORT || 3000;
 
