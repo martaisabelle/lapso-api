78e2aadde171399bbb949495e147459596263c56
Marta Isabelle <MARTALISBELLE@HOTMAIL.COM>
2026-06-17 17:11:39 -0300

    feat: create auth routes (register and login)

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+// Aula 18: rotas de autenticação
+import { Router } from 'express';
+import AuthController from '../controllers/auth.controller.js';
+
+const router = Router();
+
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
+
+export default router;
